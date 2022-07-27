416d2472b513c1cb26d7a94fcaf63359d7f52c11
Isaac Medeiros <imedeiros@uolinc.com>
2022-07-27 09:05:15 -0300

    7. Tudo em português

--- com/examples/cucumber/src/test/java/AprenderCucumber.java
+++ com/examples/cucumber/src/test/java/AprenderCucumber.java
@@ -1,6 +1,3 @@
-package features;
-
-import io.cucumber.java.PendingException;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,16 +6,16 @@ public class AprenderCucumber {
 
     @Given("^que eu criei o arquivo corretamente$")
     public void que_criei_o_arquivo_corretamente() throws Throwable {
-        throw new PendingException();
+
     }
 
     @When("^executá-lo$")
     public void executa_lo() throws Throwable {
-        throw new PendingException();
+
     }
 
     @Then("^a especificação deve finalizar com sucesso$")
-    public void a_especificacao_deve_finalizar_com_sucesso() throws Throwable {
-        throw new PendingException();
+    public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+
     }
 }
