678ca89c91646878dc0499fee429d88461078701
Isaac Medeiros <imedeiros@uolinc.com>
2022-07-27 16:07:53 -0300

    11. Asserts

--- com/examples/cucumber/src/test/java/AprenderCucumber.java
+++ com/examples/cucumber/src/test/java/AprenderCucumber.java
@@ -1,6 +1,7 @@
 import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
+import org.junit.Assert;
 
 public class AprenderCucumber {
 
@@ -10,12 +11,28 @@ public class AprenderCucumber {
     }
 
     @Quando("executá-lo")
-    public void executáLo() {
+    public void executaLo() {
 
     }
 
-    @Então("a especificação deve finalizar com sucesso")
-    public void aEspecificaçãoDeveFinalizarComSucesso() {
+    @Entao("a especificação deve finalizar com sucesso")
+    public void aEspecificacaoDeveFinalizarComSucesso() {
 
+    }
+
+    private Integer contador = 0;
+    @Dado("que o valor do contador é {int}")
+    public void queOValorDoContadorE(Integer int1) {
+        contador = int1;
+    }
+
+    @Quando("eu incrementar em {int}")
+    public void euIncrementarEm(Integer int1) {
+        contador = contador + int1;
+    }
+
+    @Entao("o valor do contador será {int}")
+    public void oValorDoContadorSera(Integer int1) {
+        Assert.assertEquals(int1, contador);
     }
 }
--- com/examples/cucumber/src/test/java/aprenderCucumber.feature
+++ com/examples/cucumber/src/test/java/aprenderCucumber.feature
@@ -8,3 +8,8 @@ Cenário: Deve executar especificação
   Dado que eu criei o arquivo corretamente
   Quando executá-lo
   Então a especificação deve finalizar com sucesso
+
+Cenario: Deve incrementar contador
+  Dado que o valor do contador é 15
+  Quando eu incrementar em 5
+  Então o valor do contador será 20
