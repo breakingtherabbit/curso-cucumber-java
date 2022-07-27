d9454e5fcf9151f3898885456e1f1b4c960c1dbc
Isaac Medeiros <imedeiros@uolinc.com>
2022-07-27 17:44:16 -0300

    12. Reaproveitando cenários - REGEX

--- com/examples/cucumber/src/test/java/AprenderCucumber.java
+++ com/examples/cucumber/src/test/java/AprenderCucumber.java
@@ -3,6 +3,11 @@ import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 import org.junit.Assert;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class AprenderCucumber {
 
     @Dado("que eu criei o arquivo corretamente")
@@ -34,5 +39,45 @@ public class AprenderCucumber {
     @Entao("o valor do contador será {int}")
     public void oValorDoContadorSera(Integer int1) {
         Assert.assertEquals(int1, contador);
+    }
+
+    Date entrega = new Date();
+    @Dado("que o prazo é dia {int}\\/{int}\\/{int}")
+    public void queOPrazoEDia(Integer dia, Integer mes, Integer ano) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dia);
+        calendar.set(Calendar.MONTH, mes - 1);
+        calendar.set(Calendar.YEAR, ano);
+        entrega = calendar.getTime();
+    }
+
+    @Quando("a entrega atrasar em {int} dias")
+    public void aEntregaAtrasarEmDias(Integer quantidadeDias) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(entrega);
+        calendar.add(Calendar.DAY_OF_MONTH, quantidadeDias);
+        entrega = calendar.getTime();
+    }
+
+    @Quando("a entrega atrasar em {int} meses")
+    public void aEntregaAtrasarEmMeses(Integer quantidadeMeses) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(entrega);
+        calendar.add(Calendar.MONTH, quantidadeMeses);
+        entrega = calendar.getTime();
+    }
+
+    @Entao("a entrega será efetuada em {int}\\/{int}\\/{int}")
+    public void aEntregaSeraEfetuadaEm(Integer dia, Integer mes, Integer ano) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, dia);
+        calendar.set(Calendar.MONTH, mes - 1);
+        calendar.set(Calendar.YEAR, ano);
+
+        DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+        String expected = format.format(calendar.getTime());
+        String actual = format.format(entrega);
+
+        Assert.assertEquals(expected, actual);
     }
 }
--- com/examples/cucumber/src/test/java/aprenderCucumber.feature
+++ com/examples/cucumber/src/test/java/aprenderCucumber.feature
@@ -13,3 +13,18 @@ Cenario: Deve incrementar contador
   Dado que o valor do contador é 15
   Quando eu incrementar em 5
   Então o valor do contador será 20
+
+Cenario: Deve incrementar contador
+  Dado que o valor do contador é 123
+  Quando eu incrementar em 35
+  Então o valor do contador será 158
+
+Cenario: Deve calcular atraso no prazo de entrega
+  Dado que o prazo é dia 05/04/2018
+  Quando a entrega atrasar em 2 dias
+  Entao a entrega será efetuada em 07/04/2018
+
+Cenario: Deve calcular atraso no prazo de entrega
+  Dado que o prazo é dia 05/04/2018
+  Quando a entrega atrasar em 2 meses
+  Entao a entrega será efetuada em 05/06/2018
